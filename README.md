4bd1d21e247845fa88a551477005ac17984a10d2
JuanDelahozMiranda <juan.delahozdevelop@gmail.com>
2021-09-11 13:51:42 -0500

    fix Tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Cypress_ApiTesting
 Proyecto para generacion de Scritp utilizando Cypress
+
+Steps:
+    1. Clone the repository
+    2. npm install cypress --save-dev 
+    3. npm run cypress:open
+    4. npm cypress run --headless -b chrome
--- integration/ApiTesting.js
+++ integration/ApiTesting.js
@@ -5,34 +5,42 @@ describe('ApiTesting_Demo', ()=>{
     //var BASE_URI = 'https://petstore.swagger.io/v2/store/order'
     //var parameters = {'id': 77, 'petId': 77, 'quantity': 3, 'shipDate': '2021-03-17', 'status': 'placed', 'complete': false}
   })
-
+  
   it('Test 1 - Request Post', ()=>{
-    var parameters = {'id': 77, 'petId': 77, 'quantity': 3, 'shipDate': '2021-03-17', 'status': 'placed', 'complete': false}
-
+    var parameters = {'id': 88, 'petId': 88, 'quantity': 3, 'shipDate': '2021-03-17', 'status': 'placed', 'complete': false}
     cy.request({method: 'POST', 
                 url:'https://petstore.swagger.io/v2/store/order', 
                 failOnStatusCode: false, 
                 headers: {'Content-Type': 'application/json'},
                 body: parameters}).then((response)=>{
-
                     expect(response).to.have.property('headers')
                     expect(response.status).to.eq(200)
-                    console.log('StatusCode: ',response.status)
                     console.log('Response_Body: ',response.body)
     })
   })
 
-  it('Test 2 - Request Get Succes', ()=>{
-
+  it('Test 2 - Request Get Fail', ()=>{
     cy.request({method: 'GET', 
-                url:'https://petstore.swagger.io/v2/store/order/77', 
+                url:'https://petstore.swagger.io/v2/store/order/99', 
+                failOnStatusCode: false, 
+                headers: {'Content-Type': 'application/json'}}).then((response)=>{
+                    expect(response.status).to.eq(404)
+                    expect(response.body).to.have.property('code', 1)
+                    expect(response.body).to.have.property('type', 'error')
+                    expect(response.body).to.have.property('message', 'Order not found')
+                    console.log('Response_Body: ',response.body)
+    })
+  })
+
+  it('Test 3 - Request Get Succes', ()=>{
+    cy.request({method: 'GET', 
+                url:'https://petstore.swagger.io/v2/store/order/88', 
                 failOnStatusCode: false, 
                 headers: {'Content-Type': 'application/json'}}).then((response)=>{
                     expect(response).to.have.property('headers')
                     expect(response.status).to.eq(200)
-                    console.log('StatusCode: ',response.status)
-                    expect(response.body).to.have.property('id', 77)
-                    expect(response.body).to.have.property('petId', 77)
+                    expect(response.body).to.have.property('id', 88)
+                    expect(response.body).to.have.property('petId', 88)
                     expect(response.body).to.have.property('quantity', 3)
                     expect(response.body['shipDate']).to.contains('2021-03-17')
                     expect(response.body).to.have.property('status', 'placed')
@@ -41,44 +49,25 @@ describe('ApiTesting_Demo', ()=>{
     })
   })
 
-  it('Test 3 - Request Get Fail', ()=>{
-
-    cy.request({method: 'GET', 
-                url:'https://petstore.swagger.io/v2/store/order/99', 
-                failOnStatusCode: false, 
-                headers: {'Content-Type': 'application/json'}}).then((response)=>{
-                    expect(response.status).to.eq(404)
-                    console.log('StatusCode: ',response.status)
-                    expect(response.body).to.have.property('code', 1)
-                    expect(response.body).to.have.property('type', 'error')
-                    expect(response.body).to.have.property('message', 'Order not found')
-                    console.log('Response_Body: ',response.body)
-    })
-  })
-
   it('Test 4 - Request Delete Success', ()=>{
-
     cy.request({method: 'DELETE', 
-                url:'https://petstore.swagger.io/v2/store/order/77', 
+                url:'https://petstore.swagger.io/v2/store/order/88', 
                 failOnStatusCode: false, 
                 headers: {'Content-Type': 'application/json'}}).then((response)=>{
                     expect(response.status).to.eq(200)
-                    console.log('StatusCode: ',response.status)
                     expect(response.body).to.have.property('code', 200)
                     expect(response.body).to.have.property('type', 'unknown')
-                    expect(response.body).to.have.property('message', '77')
+                    expect(response.body).to.have.property('message', '88')
                     console.log('Response_Body: ',response.body)
     })
   })
 
   it('Test 5 - Request Delete Fail', ()=>{
-
     cy.request({method: 'DELETE', 
                 url:'https://petstore.swagger.io/v2/store/order/99', 
                 failOnStatusCode: false, 
                 headers: {'Content-Type': 'application/json'}}).then((response)=>{
                     expect(response.status).to.eq(404)
-                    console.log('StatusCode: ',response.status)
                     expect(response.body).to.have.property('code', 404)
                     expect(response.body).to.have.property('type', 'unknown')
                     expect(response.body).to.have.property('message', 'Order Not Found')
